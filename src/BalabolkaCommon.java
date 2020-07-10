d605abdffda6a258de1af0989c099a7ec387da74
Your Name <you@example.com>
2020-07-10 10:14:38 +0300

    2020.07.10

--- src/BalabolkaCommon.java
+++ src/BalabolkaCommon.java
@@ -9,7 +9,7 @@ public class BalabolkaCommon
 
         final String destinationPath = "D:/AAA/";
         final String sourcePath = "D:\\lockelt\\21 Of Power\\";
-        final String sourceFolder = "63";
+        final String sourceFolder = "64";
 
         StringBuilder sb = new StringBuilder();
 
